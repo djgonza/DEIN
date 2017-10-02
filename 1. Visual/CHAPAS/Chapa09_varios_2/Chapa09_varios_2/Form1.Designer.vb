7dfdd7fe5614d1eac1a684f3f21833b3dd928cc6
djgonza <davidjimenezgonza@gmail.com>
2017-10-02 12:43:42 +0200

    UPDATE DEIN

---
+++ 1. Visual/CHAPAS/Chapa09_varios_2/Chapa09_varios_2/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frnVarios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnIsNumeric = New System.Windows.Forms.Button()
+        Me.lblOne = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btnIsNumeric
+        '
+        Me.btnIsNumeric.Location = New System.Drawing.Point(132, 13)
+        Me.btnIsNumeric.Name = "btnIsNumeric"
+        Me.btnIsNumeric.Size = New System.Drawing.Size(75, 23)
+        Me.btnIsNumeric.TabIndex = 2
+        Me.btnIsNumeric.Text = "isNumeric"
+        Me.btnIsNumeric.UseVisualStyleBackColor = True
+        '
+        'lblOne
+        '
+        Me.lblOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOne.Location = New System.Drawing.Point(13, 40)
+        Me.lblOne.Name = "lblOne"
+        Me.lblOne.Size = New System.Drawing.Size(100, 23)
+        Me.lblOne.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(132, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(132, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnRandom
+        '
+        Me.btnRandom.Location = New System.Drawing.Point(13, 66)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(100, 23)
+        Me.btnRandom.TabIndex = 5
+        Me.btnRandom.Text = "Generar aleatorio"
+        Me.btnRandom.UseVisualStyleBackColor = True
+        '
+        'frnVarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 390)
+        Me.Controls.Add(Me.btnRandom)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.lblOne)
+        Me.Controls.Add(Me.btnIsNumeric)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "frnVarios"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnIsNumeric As System.Windows.Forms.Button
+    Friend WithEvents lblOne As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnRandom As System.Windows.Forms.Button
+
+End Class
---
+++ 1. Visual/CHAPAS/Chapa09_varios_2/Chapa09_varios_2/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class frnVarios
+
+    Private Sub btnIsNumeric_Click(sender As System.Object, e As System.EventArgs) Handles btnIsNumeric.Click
+        TextBox2.PasswordChar = ""
+
+        If IsNumeric(TextBox1.Text) Then
+            MessageBox.Show("ES NUMÉRICO!!")
+        End If
+    End Sub
+
+    Private Sub TextBox1_GotFocus(sender As Object, e As System.EventArgs) Handles TextBox1.GotFocus
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        'MessageBox.Show("Has pulsado una puta tecla")
+        'MessageBox.Show(e.KeyChar)
+
+        'Cuando se presiona ENTER, pone el valor del TEXTBOX1 en la label y manda el foco al TEXTBOX2
+        If AscW(e.KeyChar) = Keys.Enter Then
+            lblOne.Text = TextBox1.Text
+            TextBox2.Focus()
+        End If
+
+        'Solo aparecen las letras en mayusculas => If Not (e.KeyChar Like "[A-Z]") Then
+        'Solo aparecen las letras en mayusculas, la "Ñ" y 8 => [A-ZÑ 8]
+        'Sole aparecen las letras en mayusculas y retroceso(e.KeyChar <> Convert.ToChar(Keys.Back))
+        If Not (e.KeyChar Like "[A-Z]") And e.KeyChar <> Convert.ToChar(Keys.Back) Then
+            e.Handled = True
+
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles TextBox1.TextChanged
+        'MessageBox.Show("Has cambiado tiiiiiio, antes molabas")
+    End Sub
+
+
+    Private Sub TextBox1_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles TextBox1.Validating
+        If TextBox1.Text <> "PATATA" Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    
+    Private Sub btnRandom_Click(sender As System.Object, e As System.EventArgs) Handles btnRandom.Click
+        'Label1.Text = Rnd()
+        'Genera números random entre 4 - 6 => FORMULA: Int(menor + Rnd() * (mayor - menor + 1))
+        'Label1.Text = Int(4 + Rnd() * 3)
+
+        'Otra forma de generar randoms entre 4 - 6
+        Dim numRandom As New Random
+        Dim a As Integer
+        a = numRandom.Next(4, 7)
+        Label1.Text = a
+    End Sub
+
+    Private Sub frnVarios_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Randomize()
+    End Sub
+End Class
