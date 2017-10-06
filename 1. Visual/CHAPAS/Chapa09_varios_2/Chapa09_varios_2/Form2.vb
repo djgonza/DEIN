ef182b58639f299bd9a7ce834fa44740fcab1798
djgonza <davidjimenezgonza@gmail.com>
2017-10-06 11:49:35 +0200

    UPDATE DEIN

---
+++ 1. Visual/CHAPAS/Chapa09_varios_2/Chapa09_varios_2/Form2.vb
@@ -0,0 +1,15 @@
+﻿'Option Explicit Off
+'Sirve para desactivar la opcion que obliga a declarar todas las variables
+
+'Option Strict On
+'Si esta opcion está activa, obliga a parsear
+
+Public Class Form2
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim a As Integer
+        a = TextBox1.Text
+        MessageBox.Show(a)
+
+    End Sub
+End Class
