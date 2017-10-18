4cb88e519fc92f57ed827e825db08bdba66577a3
djgonza <davidjimenezgonza@gmail.com>
2017-10-18 13:19:54 +0200

    UPDATE DEIN

---
+++ 1. Visual/CHAPAS/Chapa 14 - Varios3/Chapa 14 - Varios3/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDato = New System.Windows.Forms.TextBox()
+        Me.btnSeleccion = New System.Windows.Forms.Button()
+        Me.btnInStr = New System.Windows.Forms.Button()
+        Me.txtDato2 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtUpdate = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtDato
+        '
+        Me.txtDato.Location = New System.Drawing.Point(13, 13)
+        Me.txtDato.Name = "txtDato"
+        Me.txtDato.Size = New System.Drawing.Size(207, 20)
+        Me.txtDato.TabIndex = 0
+        Me.txtDato.Text = "patata"
+        '
+        'btnSeleccion
+        '
+        Me.btnSeleccion.Location = New System.Drawing.Point(240, 13)
+        Me.btnSeleccion.Name = "btnSeleccion"
+        Me.btnSeleccion.Size = New System.Drawing.Size(75, 23)
+        Me.btnSeleccion.TabIndex = 1
+        Me.btnSeleccion.Text = "Selecciono"
+        Me.btnSeleccion.UseVisualStyleBackColor = True
+        '
+        'btnInStr
+        '
+        Me.btnInStr.Location = New System.Drawing.Point(240, 43)
+        Me.btnInStr.Name = "btnInStr"
+        Me.btnInStr.Size = New System.Drawing.Size(75, 23)
+        Me.btnInStr.TabIndex = 2
+        Me.btnInStr.Text = "inSTr"
+        Me.btnInStr.UseVisualStyleBackColor = True
+        '
+        'txtDato2
+        '
+        Me.txtDato2.Location = New System.Drawing.Point(13, 40)
+        Me.txtDato2.Name = "txtDato2"
+        Me.txtDato2.Size = New System.Drawing.Size(207, 20)
+        Me.txtDato2.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 67)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(207, 223)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'txtUpdate
+        '
+        Me.txtUpdate.Location = New System.Drawing.Point(196, 299)
+        Me.txtUpdate.Name = "txtUpdate"
+        Me.txtUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.txtUpdate.TabIndex = 5
+        Me.txtUpdate.Text = "Update"
+        Me.txtUpdate.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(227, 67)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(258, 223)
+        Me.RichTextBox1.TabIndex = 6
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 334)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.txtUpdate)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtDato2)
+        Me.Controls.Add(Me.btnInStr)
+        Me.Controls.Add(Me.btnSeleccion)
+        Me.Controls.Add(Me.txtDato)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDato As System.Windows.Forms.TextBox
+    Friend WithEvents btnSeleccion As System.Windows.Forms.Button
+    Friend WithEvents btnInStr As System.Windows.Forms.Button
+    Friend WithEvents txtDato2 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtUpdate As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ 1. Visual/CHAPAS/Chapa 14 - Varios3/Chapa 14 - Varios3/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    'Primera parte de la explicacion
+    'Private Sub Form1_Activated(sender As Object, e As System.EventArgs) Handles Me.Activated
+    '    Debug.Print("Activated")
+    'End Sub
+
+    'Private Sub Form1_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+    '    Debug.Print("Closed")
+    'End Sub
+
+    'Private Sub Form1_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+    '    Debug.Print("Closing")
+    'End Sub
+
+    'Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    '    Debug.Print("Load")
+    'End Sub
+
+    'Private Sub Form1_Shown(sender As Object, e As System.EventArgs) Handles Me.Shown
+    '    Debug.Print("Shown")
+    'End Sub
+
+    'Private Sub Form1_Resize(sender As Object, e As System.EventArgs) Handles Me.Resize
+    '    Debug.Print("Resize")
+    'End Sub
+
+    'Private Sub Form1_Paint(sender As Object, e As System.EventArgs) Handles Me.Paint
+    '    Debug.Print("Paint")
+    'End Sub
+
+    Private Sub btnSeleccion_Click(sender As System.Object, e As System.EventArgs) Handles btnSeleccion.Click
+        'txtDato.SelectionStart = 1
+        'txtDato.SelectionLength = 3
+        'txtDato.Focus()
+        'MessageBox.Show(txtDato.SelectedText)
+
+        'Forma más comoda de poner lo anterior
+        'With txtDato
+        '    .SelectionStart = 1
+        '    .SelectionLength = 3
+        '    .Focus()
+        'End With
+
+        'Otra forma
+        txtDato.Select(1, 3)
+        txtDato.Focus()
+        MessageBox.Show(txtDato.SelectedText.Length)
+    End Sub
+
+    Private Sub btnInStr_Click(sender As System.Object, e As System.EventArgs) Handles btnInStr.Click
+        'MessageBox.Show(InStr(txtDato2.Text, ","))
+        'MessageBox.Show(txtDato2.Text.IndexOf(","c))
+        MessageBox.Show(txtDato2.Text.LastIndexOf(","c))
+    End Sub
+
+    Private Sub txtDato2_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtDato2.KeyPress
+        If Not (e.KeyChar Like "[0-9,]") And AscW(e.KeyChar) <> Keys.Back Then
+            e.Handled = True
+        Else
+            If e.KeyChar = "," And txtDato2.Text.IndexOf(","c) <> -1 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub txtDato2_Validated(sender As Object, e As System.EventArgs) Handles txtDato2.Validated
+        txtDato2.Text = FormatNumber(txtDato2.Text, 2)
+    End Sub
+
+    Private Sub txtDato2_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtDato2.Validating
+        If Not IsNumeric(txtDato2.Text) Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles txtUpdate.Click
+        PictureBox1.Image = Image.FromFile(Application.StartupPath & "\Cosas\cuervo.jpg")
+        RichTextBox1.LoadFile(Application.StartupPath & "\Cosas\cuervo.rtf")
+    End Sub
+End Class
